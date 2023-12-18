c3706a8d3f77a2af8c14ecdcff63303ec4942538
AnastasiaPodsnezhnova <152386788+AnastasiaPodsnezhnova@users.noreply.github.com>
2023-12-18 16:55:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import getdir
+
+if __name__ == "__main__":
+    # Путь к основной папке (замените на ваш путь)
+    base_folder = '/home/linx/hello/alpha'  # Пример для папки A
+
+    getdir(base_folder)

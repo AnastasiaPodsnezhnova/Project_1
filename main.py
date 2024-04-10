10300ae7356ae41730b8a9919f3ccfc3eb8f99d8
AlexandraZakharenkova <151626157+AlexandraZakharenkova@users.noreply.github.com>
2024-04-10 16:33:38 +0000

    написала симуляцию движения машинки

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ class RoadTrafficSimulation:
     self.n = n
     self.signals = ["G", "O", "R"]
     self.signal_cycle = [5, 1, 5]
+
+
+def change_light(self):
+  pass
+
+
+def simulate(self):
+  result = [self.road]
+
+  for i in range(self.n):
+    new_road = list(result[i])
+    j = -1  # переменная цикла для while
+    c_ind = new_road.index("C")  # текущий индекс машины
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-from pprint import pprint  # импорт модуля pprint для красивого вывода
+  from pprint import pprint  # импорт модуля pprint для красивого вывода
 
 
-class RoadTrafficSimulation:
+  class RoadTrafficSimulation:
 
-  def __init__(self, road, n):
-    self.road = road
-    self.n = n
-    self.signals = ["G", "O", "R"]
-    self.signal_cycle = [5, 1, 5]
+    def __init__(self, road, n):
+      self.road = road
+      self.n = n
+      self.signals = ["G", "O", "R"]
+      self.signal_cycle = [5, 1, 5]
 
   def change_light(self):
     pass

c6004332cda674e94ae1e7e704951fc0a5ee852f
Ksu <152175345+ksukopaeva@users.noreply.github.com>
2024-04-10 11:50:40 +0000

    создала класс и добавила метод конструктора

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(2 + 2)
+from pprint import pprint  # импорт модуля pprint для красивого вывода
+
+class RoadTrafficSimulation:
+
+    def __init__(self, road, n):
+        self.road = road
+        self.n = n
+        self.signals = ["G", "O", "R"]
+        self.signal_cycle = [5, 1, 5]

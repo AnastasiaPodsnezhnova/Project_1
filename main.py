bcd6efc399a8117bd468fe1a8557d76d57c4486d
Ksu <152175345+ksukopaeva@users.noreply.github.com>
2024-04-11 20:04:10 +0000

    end code

--- main.py
+++ main.py
@@ -36,8 +36,46 @@ class RoadTrafficSimulation:
 
             if n < 1:
                 color = self.next_color(color)
-                self.lights[key] = color, self.lights_time[color]
+                self.lights[key] = color,         self.lights_time[color]
             else:
                 self.lights[key] = color, n
 
         return "".join(road)
+
+def simulate(self):
+  roads = [self.road]
+
+  self.get_lights()
+
+  for i in range(self.n):
+      result = roads[-1]  # Получаем последний результат симуляции
+      result = list(self.update_lights(result))  # Изменение светофоров
+
+      for car_index, char in enumerate(result):
+          if char == "C":  # движение машины
+              if car_index + 1 < len(self.road) - 1:
+                  if result[car_index + 1] == ".":
+                      result[car_index], result[car_index + 1] = result[car_index + 1], result[car_index]
+
+                      if self.prev_char == "G":
+                          result[car_index] = self.prev_char
+                          self.prev_char = ""
+                  elif result[car_index + 1] == "G":
+                      result[car_index], result[car_index + 1] = result[car_index + 1], result[car_index]
+                      result[car_index] = "."
+                      self.prev_char = "G"
+                  break
+
+      roads.append("".join(result))  # Добавляем результат симуляции в список
+
+  return roads
+
+
+# road = "C................." # пустая дорога
+road = "C...R...R.R..G....G"  # дорога со светофорами
+n = 15
+simulation = RoadTrafficSimulation(road, n)
+results = simulation.simulate()
+
+for result in results:
+print(result)
